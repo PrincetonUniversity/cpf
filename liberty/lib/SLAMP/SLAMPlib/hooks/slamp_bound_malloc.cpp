385f348cf2926939101eefde8f2d26ef155a7461
Sotiris Apostolakis <sapostolakis@princeton.edu>
2018-12-29 03:50:19 -0500

    Add code for collaborative analysis framework, several profilers, small opts and various utilities

---
+++ liberty/lib/SLAMP/SLAMPlib/hooks/slamp_bound_malloc.cpp
@@ -0,0 +1,172 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+// Round-up/-down to a power of two    
+#define ROUND_DOWN(n,k)   ( (~((k)-1)) & (uint64_t) (n) )    
+#define ROUND_UP(n,k)     ROUND_DOWN( (n) + ((k) - 1), (k))    
+    
+// Align all allocation units to a multiple of this.    
+// MUST BE A POWER OF TWO    
+#define ALIGNMENT         (16)  
+
+// GET SIZE OF THE OBJECT
+#define GET_SIZE(ptr) *( (size_t*)( (uint64_t)(ptr) - sizeof(size_t) ) )
+#define RESET_SIZE(ptr, sz) ( *( (size_t*)( (uint64_t)(ptr) - sizeof(size_t) ) ) = (sz) )
+
+namespace slamp
+{
+
+const static size_t unit_sz = 0x100000000L;
+static size_t unit_mask;
+static uint64_t heap_begin;
+static uint64_t heap_end;
+static uint64_t heap_next;
+static size_t pagesize;
+static size_t pagemask;
+
+/* make bound_malloc to return the address from the next page */
+
+void init_bound_malloc(void* heap_bound)
+{
+  unit_mask = ~(unit_sz-1);
+
+  uint64_t a = reinterpret_cast<uint64_t>(sbrk(0));
+  heap_begin = ( a + (unit_sz-1) ) & unit_mask;
+  heap_end = reinterpret_cast<uint64_t>(heap_bound);
+
+  for (uint64_t addr = heap_begin ; addr < heap_end ; addr += unit_sz)
+  {
+    void* p = mmap(reinterpret_cast<void*>(addr), unit_sz, PROT_WRITE|PROT_READ, MAP_PRIVATE|MAP_ANONYMOUS|MAP_FIXED, -1, 0);
+    if ( p == MAP_FAILED)
+    {
+      //fprintf(stderr, "mmap failed with %p\n", (void*)addr);
+      //perror("mmap failed for bound_malloc_init\n");
+      //exit(0);
+      heap_end = addr;
+      break;
+    }
+  }
+
+  heap_next = heap_begin;
+
+  // remember pagesize to support bound_discard_page()
+  pagesize = getpagesize();
+  pagemask = ~(pagesize-1);
+
+  //fprintf(stderr, "init_bound_malloc %lx to %lx\n", heap_begin, heap_end);
+}
+
+void fini_bound_malloc()
+{
+  for (uint64_t addr = heap_begin ; addr < heap_end ; addr += unit_sz)
+    munmap( (void*)addr, unit_sz );
+}
+
+size_t get_object_size(void* ptr)
+{
+  return GET_SIZE(ptr);
+}
+
+void* bound_malloc(size_t size)
+{
+  // allocation unit size
+  size_t sz = ROUND_UP(size, ALIGNMENT);  
+
+  // store size of the unit
+  size_t* sz_ptr = (size_t*)(heap_next);
+  *sz_ptr = sz;
+  heap_next += sizeof(size_t);
+
+  void* ret = (size_t*)(heap_next);
+  heap_next += sz;
+
+  if (heap_next >= heap_end)
+  {
+    perror("Error: bound_malloc, not enough memory\n");
+    exit(0);
+  }
+
+  return ret;
+}
+
+void bound_free(void* ptr)
+{
+  // do nothing for now
+}
+
+void* bound_calloc(size_t num, size_t size)
+{
+  void* ptr = bound_malloc(num*size);
+  memset(ptr, '\0', num*size);
+  return ptr;
+}
+
+void* bound_realloc(void* ptr, size_t size)
+{
+  if (ptr == NULL)
+  {
+    /*
+       In case that ptr is a null pointer, the function behaves like malloc, assigning
+       a new block of size bytes and returning a pointer to its beginning.
+     */
+    return bound_malloc(size);
+  }
+
+  if (size == 0)
+  {
+    /*
+       If size is zero, the return value depends on the particular library
+       implementation (it may or may not be a null pointer), but the returned
+       pointer shall not be used to dereference an object in any case.
+     */
+    bound_free(ptr);
+    return NULL;
+  }
+
+  size_t old_sz = GET_SIZE(ptr);
+  if (old_sz >= size)
+  {
+    RESET_SIZE(ptr, size);
+    return ptr;   
+  }
+  else
+  { 
+    // do the same thing as malloc
+    size_t sz = ROUND_UP(size, ALIGNMENT);  
+
+    size_t* sz_ptr = (size_t*)(heap_next);
+    *sz_ptr = sz;
+    heap_next += sizeof(size_t);
+
+    void* new_ptr = (size_t*)(heap_next);
+    heap_next += sz;
+
+    // if there is not enough memory, just return the original pointer
+    if (heap_next > heap_end)
+    {
+      heap_next -= sz;
+      heap_next -= sizeof(size_t);
+      return ptr;
+    }
+
+    memcpy(new_ptr, ptr, old_sz);
+    return new_ptr;
+  }
+}
+
+void bound_discard_page()
+{
+  heap_next = ( heap_next + (pagesize-1) ) & pagemask;
+  if (heap_next >= heap_end)
+  {
+    perror("Error: bound_malloc, not enough memory\n");
+    exit(0);
+  }
+}
+
+} // namespace slamp
---
+++ liberty/lib/SLAMP/SlampOracleAA.cpp
@@ -0,0 +1,267 @@
+#define DEBUG_TYPE "slamp-oracle-aa"
+
+#include "liberty/SLAMP/SlampOracleAA.h"
+
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/IntrinsicInst.h"
+
+namespace liberty
+{
+
+using namespace llvm;
+
+STATISTIC(numQueries,       "Num queries");
+STATISTIC(numEligible,      "Num eligible queries");
+STATISTIC(numNoForwardFlow, "Num forward no-flow results");
+STATISTIC(numNoReverseFlow, "Num reverse no-flow results");
+
+static cl::opt<unsigned> Threshhold(
+  "slamp-oracle-threshhold", cl::init(0),
+  cl::NotHidden,
+  cl::desc("Maximum number of observed flows to report NoModRef"));
+
+LoopAA::AliasResult SlampOracle::alias(
+    const Value *ptrA, unsigned sizeA,
+    TemporalRelation rel,
+    const Value *ptrB, unsigned sizeB,
+    const Loop *L)
+{
+  return LoopAA::alias(ptrA,sizeA, rel, ptrB,sizeB, L);
+}
+
+LoopAA::ModRefResult SlampOracle::modref(
+  const Instruction *A,
+  TemporalRelation rel,
+  const Value *ptrB, unsigned sizeB,
+  const Loop *L)
+{
+  //std::string space(getDepth()+1, ' ');
+  //errs() << space << "si\n";
+  return LoopAA::modref(A,rel,ptrB,sizeB,L);
+}
+
+bool isMemIntrinsic(const Instruction *inst)
+{
+  return isa< MemIntrinsic >(inst);
+}
+
+bool intrinsicMayRead(const Instruction *inst)
+{
+  ImmutableCallSite cs(inst);
+  StringRef  name = cs.getCalledFunction()->getName();
+  if( name == "llvm.memset.p0i8.i32"
+  ||  name == "llvm.memset.p0i8.i64" )
+    return false;
+
+  return true;
+}
+
+LoopAA::ModRefResult SlampOracle::modref(
+  const Instruction *A,
+  TemporalRelation rel,
+  const Instruction *B,
+  const Loop *L)
+{
+  ++numQueries;
+
+  // Slamp profile data is loop sensitive.
+  if( !L || !slamp->isTargetLoop(L) )
+  {
+    // Inapplicable
+    //std::string space(getDepth()+1, ' ');
+    //errs() << space << "si\n";
+    return LoopAA::modref(A,rel,B,L);
+  }
+
+  // both instructions should be included in the target loop
+  bool includeA = false;
+  bool includeB = false;
+
+  for (Loop::block_iterator bi = L->block_begin() ; bi != L->block_end() ; bi++)
+  {
+    if ( *bi == A->getParent() )
+      includeA = true;
+    if ( *bi == B->getParent() )
+      includeB = true;
+  }
+
+  if ( !includeA || !includeB )
+  {
+    // Inapplicable
+    //std::string space(getDepth()+1, ' ');
+    //errs() << space << "si\n";
+    return LoopAA::modref(A,rel,B,L);
+  }
+
+  ModRefResult result = ModRef;
+
+  // Loop carried forward queries, or
+  // Same queries.
+  if( rel == Before || rel == Same )
+  {
+    // Slamp profile data is colected for loads, stores, and callistes.
+    // Slamp only collect FLOW info.
+    // Thus, for Before/Same queries, we are looking
+    // for Store/CallSite -> Load/CallSite
+
+    if( isa<StoreInst>(A) )
+      // Stores don't ref
+      result = Mod;
+
+    else if( isMemIntrinsic(A) )
+    {
+      if( intrinsicMayRead(A) )
+        result = ModRef;
+      else
+        result = Mod;
+    }
+
+    else if( isa<CallInst>(A) )
+    {
+      result = ModRef;
+    }
+    else
+    {
+      // inapplicable
+      //std::string space(getDepth()+1, ' ');
+      //errs() << space << "si\n";
+      result = LoopAA::modref(A,rel,B,L);
+      return result;
+    }
+
+    if( isa<LoadInst>(B) )
+    {
+      // okay
+    }
+    else if( isMemIntrinsic(B) && intrinsicMayRead(B) )
+    {
+      // okay
+    }
+    else if ( isa<CallInst>(B) )
+    {
+      // okay
+    }
+    else
+    {
+      // inapplicable, as Slamp does not collect output dependence
+      //std::string space(getDepth()+1, ' ');
+      //errs() << space << "si\n";
+      result = ModRefResult(result & LoopAA::modref(A,rel,B,L) );
+      return result;
+    }
+
+    if( rel == Before )
+    {
+      ++numEligible;
+      // Query profile data
+      if( slamp->numObsInterIterDep(L->getHeader(), B, A) <= Threshhold )
+      {
+        // No flow.
+        result = ModRefResult(result & ~Mod);
+        ++numNoForwardFlow;
+      }
+    }
+
+    else if( rel == Same )
+    {
+      ++numEligible;
+      // Query profile data
+      if( slamp->numObsIntraIterDep(L->getHeader(), B, A) <= Threshhold )
+      {
+        // No flow
+        result = ModRefResult(result & ~Mod);
+        ++numNoForwardFlow;
+      }
+    }
+  }
+
+  // Loop carried reverse queries.
+  else if( rel == After )
+  {
+    // Slamp profile data is colected for loads, stores, and callistes.
+    // Slamp only collect FLOW info.
+    // Thus, for After queries, we are looking
+    // for Store/CallSite -> Load/CallSite
+
+    // Lamp profile data is only collected for
+    // loads and stores; not callsites.
+    // Lamp collects FLOW and OUTPUT info, but
+    // not ANTI or FALSE dependence data.
+    // Thus, for After queries, we are looking
+    // for Load/CallSite -> Store/CallSite
+
+    if( isa<LoadInst>(A) )
+      // Anti or False: inapplicable
+      result = Ref;
+
+    else if( isMemIntrinsic(A) && intrinsicMayRead(A) )
+      result = ModRef;
+
+    else if( isa<CallInst>(A) )
+      result = ModRef;
+
+    else
+    {
+      // inapplicable
+      //std::string space(getDepth()+1, ' ');
+      //errs() << space << "si\n";
+      result = LoopAA::modref(A,rel,B,L);
+      return result;
+    }
+
+    // Again, only (Load/Callsite) vs (Store/CallSite)
+    if( isa<StoreInst>(B) )
+    {
+      // good
+    }
+    else if( isMemIntrinsic(B) )
+    {
+      // good
+    }
+    else if ( isa<CallInst>(B) )
+    {
+      // good
+    }
+    else
+    {
+      // inapplicable
+      //std::string space(getDepth()+1, ' ');
+      //errs() << space << "si\n";
+      result = ModRefResult(result & LoopAA::modref(A,rel,B,L));
+      return result;
+    }
+
+    ++numEligible;
+    // Query profile data.
+    if( slamp->numObsInterIterDep(L->getHeader(), A, B) <= Threshhold )
+    {
+      result = ModRefResult(result & ~Mod);
+      ++numNoReverseFlow;
+    }
+  }
+
+  if( result != NoModRef )
+  {
+    //std::string space(getDepth()+1, ' ');
+    //errs() << space << "sr " << result << "\n";
+    // Chain.
+    LoopAA::ModRefResult chainresult = LoopAA::modref(A,rel,B,L);
+
+    if ( result > ( result & chainresult ) )
+    {
+      errs() << "result " << result << "\n";
+      errs() << "chainresult " << chainresult << "\n";
+      errs() << A->getParent()->getParent()->getName() << "::" << A->getParent()->getName();
+      A->dump();
+      errs() << B->getParent()->getParent()->getName() << "::" << B->getParent()->getName();
+      B->dump();
+      errs() << "rel: " << rel << "\n";
+      assert( false );
+    }
+    result = ModRefResult(result & LoopAA::modref(A,rel,B,L) );
+  }
+
+  return result;
+}
+
+}
