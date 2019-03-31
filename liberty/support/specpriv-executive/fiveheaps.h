b5aac93c3cf7b59d9b006fe4ca96bb6818613614
Sotiris Apostolakis <sapostolakis@princeton.edu>
2019-03-31 05:13:37 -0400

    Adjustments of privateer runtime for CPF and a couple improvements

--- liberty/support/specpriv-executive/fiveheaps.h
+++ liberty/support/specpriv-executive/fiveheaps.h
@@ -18,7 +18,7 @@ void __specpriv_free_meta(void *ptr);
 
 void *__specpriv_alloc_shared(Len size, SubHeap subheap);
 void __specpriv_free_shared(void *ptr);
-void *__specpriv_alloc_ro(Len size);
+void *__specpriv_alloc_ro(Len size, SubHeap subheap);
 void __specpriv_free_ro(void *ptr);
 void *__specpriv_alloc_local(Len size, SubHeap subheap);
 void __specpriv_free_local(void *ptr);
@@ -31,28 +31,19 @@ void __specpriv_free_unclassified(void *ptr);
 void *__specpriv_alloc_redux(Len size, SubHeap subheap, ReductionType type);
 void __specpriv_free_redux(void *ptr);
 
-void *__specpriv_alloc_worker_redux(Len size, SubHeap subheap);
+void *__specpriv_alloc_worker_redux(Len size);
 void __specpriv_free_worker_redux(void *ptr);
 
 void __specpriv_reset_local(void);
 unsigned __specpriv_num_local(void);
 
 
-// Access the list of all reduction objects.
 ReductionInfo *__specpriv_first_reduction_info(void);
-ReductionInfo *__specpriv_last_reduction_info(void);
-
-// Determine if there is a single reduction type which
-// applies universally to all reduction objects.
-ReductionType __specpriv_has_universal_reduction_type(void);
 
 // Determine the size allocated in the private
 // and redux heaps BEFORE the invocation began.
 unsigned __specpriv_sizeof_private(void);
 unsigned __specpriv_sizeof_redux(void);
-
-uint64_t __specpriv_sizeof_private_subheap(SubHeap subheap);
-uint64_t __specpriv_sizeof_redux_subheap(SubHeap subheap);
 
 // At any time, one of the two is committed, and one is partial
 
