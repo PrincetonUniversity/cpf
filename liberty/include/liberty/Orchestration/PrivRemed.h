ef4b133218ea9759b5329013ddb105388e290661
Sotiris Apostolakis <sapostolakis@princeton.edu>
2019-07-12 16:25:59 -0400

    PrivRemed header initial updates for supporting different variants of private obj

--- liberty/include/liberty/Orchestration/PrivRemed.h
+++ liberty/include/liberty/Orchestration/PrivRemed.h
@@ -1,6 +1,10 @@
 #ifndef LLVM_LIBERTY_PRIVREMED_H
 #define LLVM_LIBERTY_PRIVREMED_H
 
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/PostDominators.h"
+#include "llvm/Analysis/ScalarEvolutionExpressions.h"
+#include "llvm/IR/Dominators.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
@@ -14,14 +18,43 @@ class PrivRemedy : public Remedy {
 public:
   const StoreInst *storeI;
 
+  enum PrivRemedType {
+    Normal = 0, // PartialOverlap
+    FullOverlap
+  };
+
+  PrivRemedType type;
+
   void apply(Task *task);
   bool compare(const Remedy_ptr rhs) const;
+
+  StringRef getPrivRemedyName() const {
+    switch (type) {
+    case Normal:
+      return "priv-remedy";
+      break;
+    case FullOverlap:
+      return "priv-full-overlap-remedy";
+      break;
+    }
+  }
+
+  //StringRef getRemedyName() const { return getPrivRemedyName(); };
   StringRef getRemedyName() const { return "priv-remedy"; };
 };
 
 class PrivRemediator : public Remediator {
 public:
-  void setPDG(PDG *loopPDG) { pdg = loopPDG; }
+  PrivRemediator(ModuleLoops &ml, TargetLibraryInfo *tli)
+      : Remediator(), mloops(ml), tli(tli) {}
+
+  void setLoopPDG(PDG *loopPDG, Loop *L) {
+    pdg = loopPDG;
+    Function *f = L->getHeader()->getParent();
+    pdt = &mloops.getAnalysis_PostDominatorTree(f);
+    li = &mloops.getAnalysis_LoopInfo(f);
+    se = &mloops.getAnalysis_ScalarEvolution(f);
+  }
 
   StringRef getRemediatorName() const {
     return "priv-remediator";
@@ -32,6 +65,11 @@ public:
 
 private:
   PDG *pdg;
+  ModuleLoops &mloops;
+  TargetLibraryInfo *tli;
+  PostDominatorTree *pdt;
+  LoopInfo *li;
+  ScalarEvolution *se;
 
   bool isPrivate(const Instruction *I);
 };
