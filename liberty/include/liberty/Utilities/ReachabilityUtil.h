be04e858fa6434252dce95cd193d99a00b40f126
Sotiris Apostolakis <sapostolakis@princeton.edu>
2019-07-18 20:24:36 -0400

    Implement interprocedural reachability analysis (needed by KillFlow AA)

--- liberty/include/liberty/Utilities/ReachabilityUtil.h
+++ liberty/include/liberty/Utilities/ReachabilityUtil.h
@@ -6,10 +6,27 @@
 #include "liberty/Utilities/ModuleLoops.h"
 
 #include <vector>
+#include <unordered_map>
 
 using namespace llvm;
 
 namespace liberty {
+
+typedef std::vector<const Instruction *> RelInstsV;
+typedef std::unordered_map<const Function *, RelInstsV> Fun2RelInsts;
+
+bool isReachableIntraprocedural(const Instruction *src, const Instruction *dst,
+                                ModuleLoops &mloops);
+bool isReachableIntraprocedural(RelInstsV &srcInsts, const Instruction *dst,
+                                ModuleLoops &mloops);
+
+Fun2RelInsts populateFun2RelInsts(const Instruction *I);
+
+bool isReachableInterProcedural(Fun2RelInsts &fun2RelInsts,
+                                const Instruction *dst, ModuleLoops &mloops);
+bool isReachableInterProcedural(const Instruction *src,
+                                std::vector<const Instruction *> dsts,
+                                ModuleLoops &mloops);
 
 bool noStoreInBetween(const Instruction *firstI, const Instruction *secondI,
                       std::vector<const Instruction *> &defs,
