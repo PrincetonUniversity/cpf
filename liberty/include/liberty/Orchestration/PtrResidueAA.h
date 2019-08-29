dd48595077aad78279cb5909f8f2b5238da587bb
Sotiris Apostolakis <sapostolakis@princeton.edu>
2019-08-28 20:12:21 -0400

    Add PtrResidueAA

---
+++ liberty/include/liberty/Orchestration/PtrResidueAA.h
@@ -0,0 +1,74 @@
+// This is an adaptor class from PtrResidueSpeculationManager
+// to the AA stack.
+// It reasons about pointer-residues to give
+// no-alias results.
+#ifndef LIBERTY_SPEC_PRIV_PTR_RESIDUE_ORACLE_AA_H
+#define LIBERTY_SPEC_PRIV_PTR_RESIDUE_ORACLE_AA_H
+
+#include "llvm/IR/Instructions.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "liberty/Analysis/LoopAA.h"
+#include "liberty/Speculation/PtrResidueManager.h"
+#include "liberty/Speculation/Classify.h"
+
+namespace liberty
+{
+namespace SpecPriv
+{
+using namespace llvm;
+
+struct PtrResidueAA : public LoopAA // Not a pass!
+{
+  PtrResidueAA(const DataLayout &TD, PtrResidueSpeculationManager &man)
+    : LoopAA(), td(TD), manager(man) {}
+
+  StringRef getLoopAAName() const { return "spec-priv-ptr-residue-aa"; }
+
+  virtual AliasResult alias(
+    const Value *P1, unsigned S1,
+    TemporalRelation rel,
+    const Value *P2, unsigned S2,
+    const Loop *L);
+
+  virtual ModRefResult modref(
+    const Instruction *A,
+    TemporalRelation rel,
+    const Value *ptrB, unsigned sizeB,
+    const Loop *L);
+
+  virtual ModRefResult modref(
+    const Instruction *A,
+    TemporalRelation rel,
+    const Instruction *B,
+    const Loop *L);
+
+private:
+  const DataLayout &td;
+  PtrResidueSpeculationManager &manager;
+
+  /// Can there be an alias?  If so, report necessary assumptions
+  bool may_alias(
+    const Value *P1, unsigned S1,
+    TemporalRelation rel,
+    const Value *P2, unsigned S2,
+    const Loop *L,
+    PtrResidueSpeculationManager::Assumption &a1_out,
+    PtrResidueSpeculationManager::Assumption &a2_out) const;
+
+  /// Can there be a mod-ref?  If so, report necessary assumptions
+  bool may_modref(
+    const Instruction *A,
+    TemporalRelation rel,
+    const Value *ptrB, unsigned sizeB,
+    const Loop *L,
+    PtrResidueSpeculationManager::Assumption &a1_out,
+    PtrResidueSpeculationManager::Assumption &a2_out) const;
+};
+
+}
+}
+
+#endif
+
---
+++ liberty/lib/Orchestration/PtrResidueAA.cpp
@@ -0,0 +1,304 @@
+#define DEBUG_TYPE "spec-priv-ptr-residue-aa"
+
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/GetElementPtrTypeIterator.h"
+
+#include "liberty/Orchestration/PtrResidueAA.h"
+
+namespace liberty
+{
+namespace SpecPriv
+{
+using namespace llvm;
+
+STATISTIC(numQueries, "Num queries received");
+STATISTIC(numApplicable, "Num applicable queries");
+STATISTIC(numNoAlias, "Num no-alias / no-modref");
+STATISTIC(numBeneChecks, "Num times we had to check for benefit");
+STATISTIC(numBenefit, "Num no-alias / no-modref which require speculation");
+
+// Rotate bv to the left by N bits
+static uint16_t rol_i16(uint16_t bv, unsigned N)
+{
+  return (bv << N) | (bv >> (16-N));
+}
+
+// Signed rotation: if N>0, rotate left; otherwise, rotate right.
+static uint16_t rotate_i16(uint16_t bv, int N=1)
+{
+  N %= 16;
+  if( N < 0 )
+    N += 16;
+  return rol_i16(bv,(unsigned)N);
+}
+
+// The bit-vector captures the distinct addresses (mod 16)
+// which the pointer points-to.
+// Memory accesses are typically > 1 byte in size.
+// This spreads that bit-vector over the size of the
+// access to compute the footprint (mod 16)
+static uint16_t widen(uint16_t bv, unsigned access_size_bytes)
+{
+  if( 16 <= access_size_bytes )
+    access_size_bytes = 16;
+
+  uint16_t accum = 0;
+  for(unsigned i=0; i<access_size_bytes && accum != 0x0ffffu; ++i)
+    accum = bv | rotate_i16(accum);
+
+  return accum;
+}
+
+// Residues are independent?
+static bool residues_overlap(uint16_t bv1, unsigned size1, int correction1, uint16_t bv2, unsigned size2, int correction2)
+{
+  const uint16_t adjusted1 = rotate_i16(bv1, correction1);
+  const uint16_t adjusted2 = rotate_i16(bv2, correction2);
+
+  return 0 != (widen(adjusted1,size1) & widen(adjusted2,size2));
+}
+
+static int computeConstantOffset(const DataLayout &td, const GetElementPtrInst *gep)
+{
+  assert( gep->hasAllConstantIndices() );
+
+  gep_type_iterator gi=gep_type_begin(gep), ge=gep_type_end(gep);
+  GetElementPtrInst::const_op_iterator oi=gep->idx_begin();
+
+  int accum = 0;
+  for(; gi!=ge; ++gi, ++oi)
+  {
+    ConstantInt *ci = cast< ConstantInt >(*oi);
+    int lv = ci->getSExtValue();
+
+    accum += lv * td.getTypeAllocSize(gi.getIndexedType());
+  }
+  DEBUG(errs() << "In ``" << *gep << "'': result-base == " << accum << " bytes\n");
+  return accum;
+}
+
+static const Value *adjust_pointer(const Value *v, const DataLayout &td, /* output: */ int &correction)
+{
+  for(;;)
+  {
+    if( const CastInst *cast = dyn_cast<CastInst>(v) )
+    {
+      v = cast->getOperand(0);
+      continue;
+    }
+
+    else if( const GetElementPtrInst *gep = dyn_cast<GetElementPtrInst>(v) )
+    {
+      if( gep->hasAllConstantIndices() )
+      {
+        v = gep->getPointerOperand();
+        correction += computeConstantOffset(td,gep);
+        continue;
+      }
+    }
+
+    return v;
+  }
+}
+
+bool PtrResidueAA::may_alias(
+    const Value *P1, unsigned size1,
+    TemporalRelation rel,
+    const Value *P2, unsigned size2,
+    const Loop *L,
+    PtrResidueSpeculationManager::Assumption &ass1_out,
+    PtrResidueSpeculationManager::Assumption &ass2_out) const
+{
+  if( !L )
+    return true;
+
+  if( ! P1->getType()->isPointerTy() )
+    return true;
+  if( ! P2->getType()->isPointerTy() )
+    return true;
+
+  if( P1 == P2 )
+    return true;
+
+  ++numApplicable;
+
+  int correction1 = 0;
+  const Value *ptr1 = adjust_pointer(P1, td, correction1);
+
+  int correction2 = 0;
+  const Value *ptr2 = adjust_pointer(P2, td, correction2);
+
+  const Read &read = manager.getSpecPrivResult();
+  const Ctx *ctx = read.getCtx(L);
+
+  DEBUG(errs() << "ptr-residue-aa(" << *P1 << ", " << *P2 << ")\n");
+
+  const uint16_t residual1 = read.getPointerResiduals(ptr1, ctx);
+  DEBUG(errs() << " residue(" << *ptr1 << ") = " << residual1 << '\n');
+  if( residual1 == 0 || residual1 == 0x0ffffu )
+    return true;
+
+  const uint16_t residual2 = read.getPointerResiduals(ptr2, ctx);
+  DEBUG(errs() << " residue(" << *ptr2 << ") = " << residual2 << '\n');
+  if( residual2 == 0 || residual2 == 0x0ffffu )
+    return true;
+
+  DEBUG(
+    errs() << " Pointer ``" << *ptr1 << "'' has residuals " << residual1 << ", correction " << correction1 << '\n';
+    errs() << " Pointer ``" << *ptr2 << "'' has residuals " << residual2 << ", correction " << correction2 << '\n';
+  );
+
+  if( residues_overlap( residual1,size1,correction1, residual2,size2,correction2) )
+    return true;
+
+  ass1_out = PtrResidueSpeculationManager::Assumption(ptr1,ctx);
+  ass2_out = PtrResidueSpeculationManager::Assumption(ptr2,ctx);
+
+  DEBUG(errs() << " --> NoAlias\n");
+  ++numNoAlias;
+  return false;
+}
+
+bool PtrResidueAA::may_modref(
+  const Instruction *A,
+  TemporalRelation rel,
+  const Value *P2, unsigned S2,
+  const Loop *L,
+  PtrResidueSpeculationManager::Assumption &ass_out1,
+  PtrResidueSpeculationManager::Assumption &ass_out2) const
+{
+  if( const StoreInst *store = dyn_cast<StoreInst>(A) )
+  {
+    const Value *P1 = store->getPointerOperand();
+    unsigned S1 = td.getTypeStoreSize( store->getValueOperand()->getType() );
+
+    if( !may_alias(P1,S1,rel,P2,S2,L,ass_out1,ass_out2) )
+      return false;
+  }
+  else if( const LoadInst *load = dyn_cast<LoadInst>(A) )
+  {
+    const Value *P1 = load->getPointerOperand();
+    unsigned S1 = td.getTypeStoreSize( load->getType() );
+
+    if( !may_alias(P1,S1,rel,P2,S2,L,ass_out1,ass_out2) )
+      return false;
+  }
+
+  return true;
+}
+
+LoopAA::AliasResult PtrResidueAA::alias(
+  const Value *P1, unsigned S1,
+  TemporalRelation rel,
+  const Value *P2, unsigned S2,
+  const Loop *L)
+{
+  ++numQueries;
+
+  PtrResidueSpeculationManager::Assumption a1,a2;
+  if( may_alias(P1,S1,rel,P2,S2,L,a1,a2) )
+    return LoopAA::alias(P1,S1,rel,P2,S2,L); // no help
+
+  // We can report no-alias.
+
+  // Before adding assumptions, check if the rest of the stack would have
+  // reported no-alias without this speculation.
+  if( !manager.isAssumed(a1) || !manager.isAssumed(a2) )
+  {
+    ++numBeneChecks;
+    if( NoAlias == LoopAA::alias(P1,S1,rel,P2,S2,L) )
+      return NoAlias; // speculation was not necessary.
+  }
+
+  // Speculation is required to report no-alias.
+  // Record this requirement.
+  manager.setAssumed(a1);
+  manager.setAssumed(a2);
+  ++numBenefit;
+  return NoAlias;
+}
+
+LoopAA::ModRefResult PtrResidueAA::modref(
+  const Instruction *A,
+  TemporalRelation rel,
+  const Value *P2, unsigned S2,
+  const Loop *L)
+{
+  ++numQueries;
+
+  PtrResidueSpeculationManager::Assumption a1,a2;
+  if( may_modref(A,rel,P2,S2,L,a1,a2) )
+    return LoopAA::modref(A,rel,P2,S2,L); // no help
+
+  // We can report no-mod-ref.
+
+  // Before adding assumptions, check if the rest of the stack would have
+  // reported no-mod-ref without this speculation.
+  if( !manager.isAssumed(a1) || !manager.isAssumed(a2) )
+  {
+    ++numBeneChecks;
+    if( NoModRef == LoopAA::modref(A,rel,P2,S2,L) )
+      return NoModRef; // speculation was not necessary.
+  }
+
+  // Speculation is required to report no-mod-ref.
+  // Record this requirement.
+  manager.setAssumed(a1);
+  manager.setAssumed(a2);
+  ++numBenefit;
+  return NoModRef;
+}
+
+LoopAA::ModRefResult PtrResidueAA::modref(
+  const Instruction *A,
+  TemporalRelation rel,
+  const Instruction *B,
+  const Loop *L)
+{
+  ++numQueries;
+
+  PtrResidueSpeculationManager::Assumption a1,a2;
+  const Value *P2=0;
+  unsigned S2=0;
+  if( const StoreInst *store = dyn_cast<StoreInst>(B) )
+  {
+    P2 = store->getPointerOperand();
+    S2 = td.getTypeStoreSize( store->getValueOperand()->getType() );
+  }
+
+  else if( const LoadInst *load = dyn_cast<LoadInst>(B) )
+  {
+    P2 = load->getPointerOperand();
+    S2 = td.getTypeStoreSize( load->getType() );
+  }
+
+  if( P2 )
+  {
+    if( may_modref(A,rel,P2,S2,L,a1,a2) )
+      return LoopAA::modref(A,rel,B,L); // no help
+
+    // We can report no-mod-ref.
+
+    // Before adding assumptions, check if the rest of the stack would have
+    // reported no-mod-ref without this speculation.
+    if( !manager.isAssumed(a1) || !manager.isAssumed(a2) )
+    {
+      ++numBeneChecks;
+      if( NoModRef == LoopAA::modref(A,rel,B,L) )
+        return NoModRef; // speculation was not necessary.
+    }
+
+    // Speculation is required to report no-mod-ref.
+    // Record this requirement.
+    manager.setAssumed(a1);
+    manager.setAssumed(a2);
+    ++numBenefit;
+    return NoModRef;
+  }
+
+  // All other cases.
+  return LoopAA::modref(A,rel,B,L);
+}
+
+}
+}
