2f60993bf23b5d74a21d45431df5598db9702432
Sotiris Apostolakis <sapostolakis@princeton.edu>
2019-08-28 22:34:21 -0400

    Add ReadOnlyAA and ShortLivedAA

---
+++ liberty/lib/Orchestration/ReadOnlyAA.cpp
@@ -0,0 +1,186 @@
+#define DEBUG_TYPE "spec-priv-read-only-aa"
+
+#include "llvm/ADT/SmallBitVector.h"
+#include "llvm/ADT/Statistic.h"
+
+#include "liberty/Orchestration/ReadOnlyAA.h"
+#include "liberty/Utilities/GetMemOper.h"
+
+namespace liberty
+{
+using namespace llvm;
+
+STATISTIC(numQueries,    "Num queries");
+STATISTIC(numEligible,   "Num eligible queries");
+STATISTIC(numPrivatized, "Num privatized");
+STATISTIC(numSeparated,  "Num separated");
+STATISTIC(numNoRead,     "Num no-read");
+
+LoopAA::AliasResult ReadOnlyAA::alias(
+    const Value *ptrA, unsigned sizeA,
+    TemporalRelation rel,
+    const Value *ptrB, unsigned sizeB,
+    const Loop *L) {
+
+  ++numQueries;
+
+//  if( !L || !asgn.isValidFor(L) )
+//    return LoopAA::alias(ptrA,sizeA, rel, ptrB,sizeB, L);
+
+  if( !isa<PointerType>( ptrA->getType() ) )
+    return LoopAA::alias(ptrA,sizeA, rel, ptrB,sizeB, L);
+  if( !isa<PointerType>( ptrB->getType() ) )
+    return LoopAA::alias(ptrA,sizeA, rel, ptrB,sizeB, L);
+
+  //const Ctx *ctx = read.getCtx(L);
+
+  ++numEligible;
+
+  Ptrs aus1;
+  HeapAssignment::Type t1 = HeapAssignment::Unclassified;
+  if( read.getUnderlyingAUs(ptrA,ctx,aus1) )
+    t1 = asgn.classify(aus1);
+
+  Ptrs aus2;
+  HeapAssignment::Type t2 = HeapAssignment::Unclassified;
+  if( read.getUnderlyingAUs(ptrB,ctx,aus2) )
+    t2 = asgn.classify(aus2);
+
+  if ((t1 == HeapAssignment::ReadOnly || t2 == HeapAssignment::ReadOnly) &&
+      t1 != t2 && t1 != HeapAssignment::Unclassified &&
+      t2 != HeapAssignment::Unclassified) {
+    ++numSeparated;
+    return NoAlias;
+  }
+
+  return LoopAA::alias(ptrA,sizeA, rel, ptrB,sizeB, L);
+
+}
+
+LoopAA::ModRefResult
+ReadOnlyAA::check_modref(const Value *ptrA, const Value *ptrB, const Loop *L) {
+
+  if (!ptrA || !ptrB)
+    return ModRef;
+
+  //  if( !L || !asgn.isValidFor(L) )
+//    return MayAlias;
+
+  if( !isa<PointerType>( ptrA->getType() ) )
+    return ModRef;
+  if( !isa<PointerType>( ptrB->getType() ) )
+    return ModRef;
+
+  //const Ctx *ctx = read.getCtx(L);
+
+  ++numEligible;
+
+  Ptrs aus1;
+  HeapAssignment::Type t1 = HeapAssignment::Unclassified;
+  if( read.getUnderlyingAUs(ptrA,ctx,aus1) )
+    t1 = asgn.classify(aus1);
+
+  Ptrs aus2;
+  HeapAssignment::Type t2 = HeapAssignment::Unclassified;
+  if( read.getUnderlyingAUs(ptrB,ctx,aus2) )
+    t2 = asgn.classify(aus2);
+
+  if ((t1 == HeapAssignment::ReadOnly || t2 == HeapAssignment::ReadOnly) &&
+      t1 != t2 && t1 != HeapAssignment::Unclassified &&
+      t2 != HeapAssignment::Unclassified) {
+    ++numSeparated;
+    return NoModRef;
+  }
+
+  if (t1 == HeapAssignment::ReadOnly || t2 == HeapAssignment::ReadOnly) {
+    ++numNoRead;
+    return ModRefResult(~Mod);
+  }
+
+  return ModRef;
+}
+
+LoopAA::ModRefResult ReadOnlyAA::modref(
+  const Instruction *A,
+  TemporalRelation rel,
+  const Value *ptrB, unsigned sizeB,
+  const Loop *L)
+{
+
+  ++numQueries;
+
+  const Value *ptrA = liberty::getMemOper(A);
+
+  ModRefResult result = check_modref(ptrA, ptrB, L);
+
+  if( result != NoModRef )
+    // Chain.
+    result = ModRefResult(result & LoopAA::modref(A,rel,ptrB,sizeB,L) );
+
+  return result;
+}
+
+LoopAA::ModRefResult ReadOnlyAA::modref(
+  const Instruction *A,
+  TemporalRelation rel,
+  const Instruction *B,
+  const Loop *L)
+{
+
+  ++numQueries;
+
+  const Value *ptrA = liberty::getMemOper(A);
+  const Value *ptrB = liberty::getMemOper(B);
+
+  ModRefResult result = check_modref(ptrA, ptrB, L);
+
+  if( result != NoModRef )
+    // Chain.
+    result = ModRefResult(result & LoopAA::modref(A,rel,B,L) );
+
+  return result;
+}
+
+/*
+LoopAA::AliasResult ReadOnlyAA::aliasCheck(
+    const Pointer &P1,
+    TemporalRelation rel,
+    const Pointer &P2,
+    const Loop *L)
+{
+  ++numQueries;
+
+//  if( !L || !asgn.isValidFor(L) )
+//    return MayAlias;
+
+  if( !isa<PointerType>( P1.ptr->getType() ) )
+    return MayAlias;
+  if( !isa<PointerType>( P2.ptr->getType() ) )
+    return MayAlias;
+
+  //const Ctx *ctx = read.getCtx(L);
+
+  ++numEligible;
+
+  Ptrs aus1;
+  HeapAssignment::Type t1 = HeapAssignment::Unclassified;
+  if( read.getUnderlyingAUs(P1.ptr,ctx,aus1) )
+    t1 = asgn.classify(aus1);
+
+  Ptrs aus2;
+  HeapAssignment::Type t2 = HeapAssignment::Unclassified;
+  if( read.getUnderlyingAUs(P2.ptr,ctx,aus2) )
+    t2 = asgn.classify(aus2);
+
+  if ((t1 == HeapAssignment::ReadOnly || t2 == HeapAssignment::ReadOnly) &&
+      t1 != t2 && t1 != HeapAssignment::Unclassified &&
+      t2 != HeapAssignment::Unclassified) {
+    ++numSeparated;
+    return NoAlias;
+  }
+
+  return MayAlias;
+}
+*/
+
+}
---
+++ liberty/lib/Orchestration/ShortLivedAA.cpp
@@ -0,0 +1,95 @@
+#define DEBUG_TYPE "spec-priv-local-aa"
+
+#include "llvm/ADT/SmallBitVector.h"
+#include "llvm/ADT/Statistic.h"
+
+#include "liberty/Orchestration/ShortLivedAA.h"
+
+namespace liberty
+{
+using namespace llvm;
+
+STATISTIC(numQueries,    "Num queries");
+STATISTIC(numEligible,   "Num eligible queries");
+STATISTIC(numPrivatized, "Num privatized");
+STATISTIC(numSeparated,  "Num separated");
+
+LoopAA::AliasResult ShortLivedAA::aliasCheck(
+    const Pointer &P1,
+    TemporalRelation rel,
+    const Pointer &P2,
+    const Loop *L)
+{
+  ++numQueries;
+
+//  if( !L || !asgn.isValidFor(L) )
+//    return MayAlias;
+
+  if( !isa<PointerType>( P1.ptr->getType() ) )
+    return MayAlias;
+  if( !isa<PointerType>( P2.ptr->getType() ) )
+    return MayAlias;
+
+  //const Ctx *ctx = read.getCtx(L);
+
+  ++numEligible;
+
+  Ptrs aus1;
+  HeapAssignment::Type t1 = HeapAssignment::Unclassified;
+  if( read.getUnderlyingAUs(P1.ptr,ctx,aus1) )
+    t1 = asgn.classify(aus1);
+
+  Ptrs aus2;
+  HeapAssignment::Type t2 = HeapAssignment::Unclassified;
+  if( read.getUnderlyingAUs(P2.ptr,ctx,aus2) )
+    t2 = asgn.classify(aus2);
+
+  // Loop-carried queries:
+  if( rel != LoopAA::Same )
+  {
+    // local heaps are iteration-private, thus
+    // there cannot be cross-iteration flows.
+    if( t1 == HeapAssignment::Local )
+    {
+      ++numPrivatized;
+      return NoAlias;
+    }
+
+    if( t2 == HeapAssignment::Local )
+    {
+      ++numPrivatized;
+      return NoAlias;
+    }
+  }
+
+  // Both loop-carried and intra-iteration queries: are they assigned to different heaps?
+  if ((t1 == HeapAssignment::Local || t2 == HeapAssignment::Local) &&
+      t1 != t2 && t1 != HeapAssignment::Unclassified &&
+      t2 != HeapAssignment::Unclassified) {
+    ++numSeparated;
+    return NoAlias;
+  }
+
+  /*
+  // They are assigned to the same heap.
+  // Are they assigned to different sub-heaps?
+  if( t1 == t2 && t1 != HeapAssignment::Unclassified )
+  {
+    //sdflsdakjfjsdlkjfl
+    const int subheap1 = asgn.getSubHeap(aus1);
+    if( subheap1 > 0 )
+    {
+      const int subheap2 = asgn.getSubHeap(aus2);
+      if( subheap2 > 0 && subheap1 != subheap2 )
+      {
+        ++numSubSep;
+        return NoAlias;
+      }
+    }
+  }
+  */
+
+  return MayAlias;
+}
+
+}
