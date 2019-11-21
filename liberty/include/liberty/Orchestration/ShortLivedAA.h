ecfab7a9c4cfcdecc7fbea1a8fd6fba742b63433
Sotiris Apostolakis <sapostolakis@princeton.edu>
2019-11-21 04:06:49 -0500

    ShortLivedAA mods

--- liberty/include/liberty/Orchestration/ShortLivedAA.h
+++ liberty/include/liberty/Orchestration/ShortLivedAA.h
@@ -6,7 +6,8 @@
 #ifndef LIBERTY_SPEC_PRIV_SHORT_LIVED_AA_H
 #define LIBERTY_SPEC_PRIV_SHORT_LIVED_AA_H
 
-#include "liberty/Analysis/ClassicLoopAA.h"
+#include "liberty/Analysis/LoopAA.h"
+#include "liberty/Orchestration/Remediator.h"
 #include "liberty/Speculation/Classify.h"
 #include "liberty/Speculation/Read.h"
 #include "liberty/Strategy/PerformanceEstimator.h"
@@ -16,16 +17,16 @@ namespace liberty
 using namespace llvm;
 using namespace SpecPriv;
 
-struct ShortLivedAA : public ClassicLoopAA // Not a pass!
+struct ShortLivedAA : public LoopAA, Remediator // Not a pass!
 {
   ShortLivedAA(const Read &rd, const HeapAssignment &ha, const Ctx *cx,
                PerformanceEstimator *pf)
-      : ClassicLoopAA(), read(rd), asgn(&ha), localAUs(nullptr), ctx(cx),
+      : LoopAA(), read(rd), asgn(&ha), localAUs(nullptr), ctx(cx),
         perf(pf) {}
 
   ShortLivedAA(const Read &rd, const HeapAssignment::AUSet *slAUs,
                const Ctx *cx, PerformanceEstimator *pf)
-      : ClassicLoopAA(), read(rd), asgn(nullptr), localAUs(slAUs), ctx(cx),
+      : LoopAA(), read(rd), asgn(nullptr), localAUs(slAUs), ctx(cx),
         perf(pf) {}
 
   virtual SchedulingPreference getSchedulingPreference() const {
@@ -34,9 +35,30 @@ struct ShortLivedAA : public ClassicLoopAA // Not a pass!
 
   StringRef getLoopAAName() const { return "spec-priv-local-aa"; }
 
+  StringRef getRemediatorName() const { return "spec-priv-local-remed"; }
+
+  LoopAA::AliasResult alias(const Value *ptrA, unsigned sizeA,
+                            TemporalRelation rel, const Value *ptrB,
+                            unsigned sizeB, const Loop *L, Remedies &R,
+                            DesiredAliasResult dAliasRes = DNoOrMustAlias);
+
+  LoopAA::ModRefResult modref(const Instruction *A, TemporalRelation rel,
+                              const Value *ptrB, unsigned sizeB, const Loop *L,
+                              Remedies &R);
+
+  LoopAA::ModRefResult modref(const Instruction *A, TemporalRelation rel,
+                              const Instruction *B, const Loop *L, Remedies &R);
+
+  /*
   virtual AliasResult aliasCheck(const Pointer &P1, TemporalRelation rel,
                                  const Pointer &P2, const Loop *L, Remedies &R,
                                  DesiredAliasResult dAliasRes = DNoOrMustAlias);
+  */
+
+  LoopAA::ModRefResult modref_with_ptrs(const Instruction *A, const Value *ptrA,
+                                        TemporalRelation rel,
+                                        const Instruction *B, const Value *ptrB,
+                                        const Loop *L, Remedies &R);
 
 private:
   const Read &read;
@@ -44,6 +66,10 @@ private:
   const HeapAssignment::AUSet *localAUs;
   const Ctx *ctx;
   PerformanceEstimator *perf;
+
+  LoopAA::ModRefResult check_modref(const Value *ptrA, TemporalRelation rel,
+                                    const Value *ptrB, const Loop *L,
+                                    Remedies &R);
 };
 
 }
