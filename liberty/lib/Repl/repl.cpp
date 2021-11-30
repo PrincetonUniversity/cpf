45b5c6ab437c311c8f5ecb748eaad18d957a1b32
Ziyang Xu <ziyangx@princeton.edu>
2021-11-30 00:16:47 -0500

    add some features

--- liberty/lib/Repl/repl.cpp
+++ liberty/lib/Repl/repl.cpp
@@ -1,4 +1,5 @@
 #include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
 #include "liberty/Utilities/ReportDump.h"
 #include "liberty/LoopProf/Targets.h"
 #include <cctype>
@@ -43,23 +44,52 @@ void OptRepl::getAnalysisUsage(AnalysisUsage &au) const
 
 bool OptRepl::runOnModule(Module &M) {
   bool modified = false;
+  Loop *selectedLoop;
+  Function *selectedFunction;
+  map<unsigned, Loop *> loopIdMap;
   while (true) {
+    outs() << "(opt-repl) ";
     string query;
-    cin >> query;
+    getline(cin, query);
     // clean up query
 
     if (query == "loops") {
+      outs() << "List of hot loops:\n";
       ModuleLoops &mloops = getAnalysis<ModuleLoops>();
       const Targets &targets = getAnalysis<Targets>();
 
+      unsigned loopId = 0;
       for (Targets::iterator i = targets.begin(mloops),
                              e = targets.end(mloops);
            i != e; ++i) {
         Loop *loop = *i;
-        outs() << loop->getHeader()->getParent()->getName()
+        loopIdMap[loopId] = loop;
+        outs() << loopId << ": " << loop->getHeader()->getParent()->getName()
                << "::" << loop->getHeader()->getName() << '\n';
+        loopId++;
         continue;
       }
+    }
+    if (query.find("select") == 0) {
+      // keep only numbers
+      outs() << query << "\n";
+      query.erase(remove_if(query.begin(), query.end(),
+                            [](unsigned char a) { return !std::isdigit(a); }), query.end());
+
+      unsigned loopId = stoi(query);
+      if (loopIdMap.find(loopId) == loopIdMap.end()) {
+        outs() << "Loop " << loopId << " does not exist\n";
+        continue;
+      }
+      Loop *loop = loopIdMap[loopId];
+      outs() << "Selecting loop " << loopId << ": ";
+      outs() << loop->getHeader()->getParent()->getName()
+             << "::" << loop->getHeader()->getName() << '\n';
+      selectedLoop = loop;
+    }
+    if (query == "dump") {
+      outs() << *selectedLoop;
+      selectedLoop->dump();
     }
     if (query == "quit") {
       break;
