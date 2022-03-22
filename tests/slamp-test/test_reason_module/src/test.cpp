0cc729d0e63c475525ccda2af7d415f846fa1e0e
Ziyang Xu <ziyangx@princeton.edu>
2022-03-22 00:18:00 -0400

    add test for slamp dep reasoning

---
+++ tests/slamp-test/test_reason_module/src/test.cpp
@@ -0,0 +1,34 @@
+/**
+ * Test constant module of SLAMP
+ *
+ * Ziyang Xu
+ * Liberty Research Group, Princeton
+ */
+
+#include <cstdio>
+#include <cstdlib>
+
+void foo(int *ptr, int i) {
+    *ptr = 2 * i + 7;
+}
+
+int main(int argc, char **argv) {
+  unsigned iter;
+  if (argc == 2) {
+    iter = atoi(argv[1]);
+  } else {
+    printf("Need one arguments: iter\n");
+  }
+
+  auto p = new int();
+
+  // initialize value
+  auto sum = 0;
+  for (int i = 0; i < iter; i++) {
+    foo(p, i);
+    sum = (sum + *p) % 101;
+  }
+  
+  printf("%d\n", sum);
+  return 0;
+}
