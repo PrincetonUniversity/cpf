bc5c53a2b4ab31a4107f98bb112fbbe245ac246e
Ziyang Xu <ziyangx@princeton.edu>
2022-11-03 18:57:02 -0400

    refactoring

--- liberty/lib/SLAMP/SLAMPcustom/consumer/ProfilingModules/DependenceModule.h
+++ liberty/lib/SLAMP/SLAMPcustom/consumer/ProfilingModules/DependenceModule.h
@@ -1,23 +1,77 @@
 #include <cstdint>
+#include <mutex>
+#include <unordered_set>
+#include <vector>
 
-namespace DepMod {
-void init(uint32_t loop_id, uint32_t pid);
-void fini(const char *filename);
-void load(uint32_t instr, const uint64_t addr, const uint32_t bare_instr, uint64_t value);
-void store(uint32_t instr, uint32_t bare_instr, const uint64_t addr);
-void allocate(void *addr, uint64_t size);
-void loop_invoc();
-void loop_iter();
+#include "slamp_timestamp.h"
+#include "slamp_shadow_mem.h"
+#include "slamp_logger.h"
 
-enum DepModAction: char
-{
-    INIT = 0,
-    LOAD,
-    STORE,
-    ALLOC,
-    LOOP_INVOC,
-    LOOP_ITER,
-    FINISHED
+enum DepModAction : char {
+  INIT = 0,
+  LOAD,
+  STORE,
+  ALLOC,
+  LOOP_INVOC,
+  LOOP_ITER,
+  FINISHED
 };
 
-} // namespace DepMod
+class LocalWriteModule {
+  protected:
+    const uint32_t LOCALWRITE_MASK{};
+    const uint32_t LOCALWRITE_PATTERN{};
+    static constexpr uint32_t LOCALWRITE_SHIFT = 12; // PAGE SIZE 4096 = 2^12
+    // takes in a lambda action and uint64_t addr
+    template <typename F>
+    inline void local_write(uint64_t addr, const F &action) {
+        if (((addr >> LOCALWRITE_SHIFT) & LOCALWRITE_MASK) == LOCALWRITE_PATTERN) {
+            action();
+        }
+    }
+
+  public:
+    LocalWriteModule(uint32_t mask, uint32_t pattern) : LOCALWRITE_MASK(mask), LOCALWRITE_PATTERN(pattern) {}
+    virtual ~LocalWriteModule() = default;
+};
+
+class DependenceModule : public LocalWriteModule {
+private:
+  static constexpr unsigned DEPLOG_VEC_SIZE = 1'000'000;
+  static constexpr unsigned VECTOR_TO_SET_THREADS = 56;
+
+  uint64_t slamp_iteration = 0;
+  uint64_t slamp_invocation = 0;
+  uint32_t target_loop_id = 0;
+
+  // debugging stats
+  uint64_t load_count = 0;
+  uint64_t store_count = 0;
+
+  slamp::MemoryMap *smmap = nullptr;
+  std::unordered_set<slamp::KEY, slamp::KEYHash, slamp::KEYEqual> deplog_set;
+  std::vector<slamp::KEY> deplog_vec;
+  std::mutex m; // mutex for the deplog_set
+
+  void convertVectorToSet();
+  void log(TS ts, const uint32_t dst_inst, const uint32_t bare_inst,
+           const uint64_t load_invocation, const uint64_t load_iteration);
+
+public:
+
+  DependenceModule(uint32_t mask, uint32_t pattern)
+      : LocalWriteModule(mask, pattern) {
+    smmap = new slamp::MemoryMap(TIMESTAMP_SIZE_IN_BYTES);
+    deplog_vec.reserve(DEPLOG_VEC_SIZE);
+  }
+
+  void init(uint32_t loop_id, uint32_t pid);
+  void fini(const char *filename);
+  // always_inline attribute
+  void load(uint32_t instr, const uint64_t addr, const uint32_t bare_instr,
+            uint64_t value);
+  void store(uint32_t instr, uint32_t bare_instr, const uint64_t addr);
+  void allocate(void *addr, uint64_t size);
+  void loop_invoc();
+  void loop_iter();
+};
--- liberty/lib/SLAMP/SLAMPcustom/consumer/consumer.cpp
+++ liberty/lib/SLAMP/SLAMPcustom/consumer/consumer.cpp
@@ -12,17 +12,32 @@
 #include <xmmintrin.h>
 
 namespace bip = boost::interprocess;
+using Action = DepModAction;
+
+static inline uint64_t rdtsc() {
+  uint64_t a, d;
+  __asm__ volatile("rdtsc" : "=a"(a), "=d"(d));
+  return (d << 32) | a;
+}
 
 #define DEBUG 0
 #define ACTION 1
+#define MEASURE_TIME 1
+
+// #define CONSUME         sq_consume(the_queue);
+#define CONSUME         consume();
+
+static uint64_t load_time(0);
+static uint64_t store_time(0);
+static uint64_t alloc_time(0);
 
 // create segment and corresponding allocator
-bip::fixed_managed_shared_memory *segment, *segment2;
-static SW_Queue the_queue;
+bip::fixed_managed_shared_memory *segment;
 static double_queue_p dqA, dqB, dq, dq_other;
 static uint64_t dq_index = 0;
 static uint64_t dq_size = 0;
 static uint64_t *dq_data;
+
 static void swap(){
   if(dq == dqA){
     dq = dqB;
@@ -76,98 +91,28 @@ static inline void consume_32_32_64(uint32_t &x, uint32_t &y, uint64_t &z){
   // _mm_prefetch(&dq_data[dq_index] + QPREFETCH, _MM_HINT_NTA);
 }
 
-// #define CONSUME         sq_consume(the_queue);
-#define CONSUME         consume();
-#define PRODUCE(x)      sq_produce(the_queue,(uint64_t)x);
+void consume_loop() {
 
-#define PRODUCE_2(x,y)  PRODUCE( (((uint64_t)x)<<32) | (uint32_t)(y) )
-#define CONSUME_2(x,y)  do { uint64_t tmp = CONSUME; x = (uint32_t)(tmp>>32); y = (uint32_t) tmp; } while(0)
+  DependenceModule depMod(0x3, 1);
 
-int main(int argc, char** argv) {
-
-  segment = new bip::fixed_managed_shared_memory(bip::open_or_create, "MySharedMemory", sizeof(uint64_t) *QSIZE *4, (void*)(1UL << 32));
-  // segment2 = new bip::fixed_managed_shared_memory(bip::open_or_create, "MySharedMemory2", sizeof(uint64_t) *QSIZE *2, (void*)(1UL << 28));
-    // managed_shared_memory(bip::open_or_create, "MySharedMemory", sizeof(uint64_t) *QSIZE *2);
-  // auto a_queue = new atomic_queue::AtomicQueueB<shm::Element, shm::char_alloc, shm::NIL>(65536);
-
-  dqA = segment->construct<double_queue_t>("DQ_A")(double_queue_t());
-  dqB = segment->construct<double_queue_t>("DQ_B")(double_queue_t());
-  auto dataA = segment->construct<uint64_t>("DQ_Data_A")[QSIZE](uint64_t());
-  auto dataB = segment->construct<uint64_t>("DQ_Data_B")[QSIZE](uint64_t());
-  dqA->init(dataA);
-  dqB->init(dataB);
-  dq = dqB;
-  dq_other = dqA;
-  dq_data = dq->data;
-
-
-  // the_queue = static_cast<SW_Queue>(segment->find_or_construct<sw_queue_t>("MyQueue")());
-  // auto data = static_cast<uint64_t*>(segment2->find_or_construct<uint64_t>("smtx_queue_data")[QSIZE]());
-  // segment = new bip::managed_shared_memory(bip::open_or_create, "MySharedMemory", sizeof(uint64_t) *QSIZE *2);
-  // auto a_queue = new atomic_queue::AtomicQueueB<shm::Element, shm::char_alloc, shm::NIL>(65536);
-  // the_queue = static_cast<SW_Queue>(segment->find_or_construct<sw_queue_t>("MyQueue")());
-  // auto data = static_cast<uint64_t*>(segment->find_or_construct<uint64_t>("smtx_queue_data")[QSIZE]());
-
-  // // measure time
-  // auto start = std::chrono::high_resolution_clock::now();
-  // unsigned long ROUND = 100;
-  // // parse the ROUND number from cmd
-  // if (argc > 1) {
-  //   std::stringstream ss;
-  //   ss << argv[1];
-  //   ss >> ROUND;
-  // }
-  // for (int round = 0; round < ROUND; round++) {
-  //   for (unsigned long i = 0; i < QSIZE; i++) {
-  //     data[i] = i;
-  //   }
-  // }
-  // auto end = std::chrono::high_resolution_clock::now();
-  // std::chrono::duration<double> diff = end-start;
-  // std::cout << "Time for " << ROUND * QSIZE / 1'000'000 << "M events : " << diff.count() << " s" << std::endl;
-  // // throughput
-  // std::cout << "Write Throughput: " << ROUND * QSIZE / diff.count() / 1'000'000 << "M events/s" << std::endl;
-
-  // start = std::chrono::high_resolution_clock::now();
-  // unsigned total = 0;
-  // for (int round = 0; round < ROUND; round++) {
-  //   for (unsigned long i = 0; i < QSIZE; i++) {
-  //     total += data[i];
-  //   }
-  // }
-  // end = std::chrono::high_resolution_clock::now();
-  // diff = end-start;
-  // std::cout << "Total = " << total << std::endl;
-  // std::cout << "Time for " << ROUND * QSIZE / 1'000'000 << "M events : " << diff.count() << " s" << std::endl;
-  // // throughput
-  // std::cout << "Read Throughput: " << ROUND * QSIZE / diff.count() / 1'000'000 << "M events/s" << std::endl;
-
-
-  // if (the_queue == nullptr) {
-    // std::cout << "Error: could not create queue" << std::endl;
-    // return 1;
-  // }
-  // the_queue->data = data;
-  // if (the_queue->data == nullptr) {
-    // std::cout << "Error: could not create queue data" << std::endl;
-    // return 1;
-  // }
-  // [> Initialize the queue data structure <]
-  // the_queue->p_data = (uint64_t) the_queue->data;
-  // the_queue->c_inx = 0;
-  // the_queue->c_margin = 0;
-  // the_queue->p_glb_inx = 0;
-  // the_queue->c_glb_inx = 0;
-  // the_queue->ptr_c_glb_inx = &(the_queue->c_glb_inx);
-  // the_queue->ptr_p_glb_inx = &(the_queue->p_glb_inx);
-
-
+  uint64_t rdtsc_start = 0;
   uint64_t counter = 0;
-  // shm::shared_string v(char_alloc);
-
-  using Action = DepMod::DepModAction;
-
   uint32_t loop_id;
+
+  // measure time with lambda action
+  auto measure_time = [](uint64_t &time, auto action) {
+    // measure time with rdtsc
+    if (MEASURE_TIME) {
+      uint64_t start = rdtsc();
+      action();
+      uint64_t end = rdtsc();
+      time += end - start;
+    }
+    else {
+      action();
+    }
+  };
+
   while (true) {
     check();
     char v;
@@ -179,13 +124,14 @@ int main(int argc, char** argv) {
       uint32_t pid;
       loop_id = (uint32_t)CONSUME;
       pid = (uint32_t)CONSUME;
+      rdtsc_start = rdtsc();
 
       if (DEBUG) {
         std::cout << "INIT: " << loop_id << " " << pid << std::endl;
       }
-#if ACTION
-      DepMod::init(loop_id, pid);
-#endif
+      if (ACTION) {
+        depMod.init(loop_id, pid);
+      }
       break;
     };
     case Action::LOAD: {
@@ -193,18 +139,17 @@ int main(int argc, char** argv) {
       uint64_t addr;
       uint32_t bare_instr;
       uint64_t value = 0;
+
       consume_32_32_64(instr, bare_instr, addr);
-      // CONSUME_2(instr, bare_instr);
-      // addr = CONSUME;
-      // value = CONSUME;
+
       if (DEBUG) {
         std::cout << "LOAD: " << instr << " " << addr << " " << bare_instr
                   << " " << value << std::endl;
       }
-#if ACTION
-      DepMod::load(instr, addr, bare_instr, value);
-      // DepMod::load(instr, addr, bare_instr, value);
-#endif
+      if (ACTION) {
+        measure_time(load_time,
+                     [&]() { depMod.load(instr, addr, bare_instr, value); });
+      }
 
       break;
     };
@@ -213,74 +158,105 @@ int main(int argc, char** argv) {
       uint32_t bare_instr;
       uint64_t addr;
       consume_32_32_64(instr, bare_instr, addr);
-      // CONSUME_2(instr, bare_instr);
-      // addr = CONSUME;
+
       if (DEBUG) {
         std::cout << "STORE: " << instr << " " << bare_instr << " " << addr
                   << std::endl;
       }
-#if ACTION
-      DepMod::store(instr, bare_instr, addr);
-#endif
+      if (ACTION) {
+        measure_time(store_time,
+                     [&]() { depMod.store(instr, bare_instr, addr); });
+      }
       break;
     };
     case Action::ALLOC: {
       uint64_t addr;
       uint64_t size;
       consume_64_64(addr, size);
-      // addr = CONSUME;
-      // size = CONSUME;
+
       if (DEBUG) {
         std::cout << "ALLOC: " << addr << " " << size << std::endl;
       }
-#if ACTION
-      DepMod::allocate(reinterpret_cast<void *>(addr), size);
-#endif
+      if (ACTION) {
+        measure_time(alloc_time, [&]() {
+          depMod.allocate(reinterpret_cast<void *>(addr), size);
+        });
+      }
       break;
     };
     case Action::LOOP_INVOC: {
-#if ACTION
-      DepMod::loop_invoc();
-#endif
-
       if (DEBUG) {
         std::cout << "LOOP_INVOC" << std::endl;
+      }
+
+      if (ACTION) {
+        depMod.loop_invoc();
       }
       break;
     };
     case Action::LOOP_ITER: {
-#if ACTION
-      DepMod::loop_iter();
-#endif
-
       if (DEBUG) {
         std::cout << "LOOP_ITER" << std::endl;
+      }
+      if (ACTION) {
+        depMod.loop_iter();
       }
       break;
     };
     case Action::FINISHED: {
-      std::stringstream ss;
-      ss << "deplog-" << loop_id << ".txt";
-#if ACTION
-      DepMod::fini(ss.str().c_str());
-#endif
+      if (ACTION) {
+        std::stringstream ss;
+        ss << "deplog-" << loop_id << ".txt";
+        depMod.fini(ss.str().c_str());
+      }
+
+      uint64_t rdtsc_end = rdtsc();
+      // total cycles
+      uint64_t total_cycles = rdtsc_end - rdtsc_start;
       std::cout << "Finished loop: " << loop_id << " after " << counter
                 << " events" << std::endl;
+      // print time in seconds
+      std::cout << "Total time: " << total_cycles / 2.6e9 << " s" << std::endl;
+      std::cout << "Load time: " << load_time / 2.6e9 << " s" << std::endl;
+      std::cout << "Store time: " << store_time / 2.6e9 << " s" << std::endl;
+      std::cout << "Alloc time: " << alloc_time / 2.6e9 << " s" << std::endl;
+      exit(0);
+
       break;
     };
     default:
       std::cout << "Unknown action: " << (uint64_t)v << std::endl;
 
-      std::cout << "Is ready to read?:" << dq->ready_to_read << " " << "Is ready to write?:" << dq->ready_to_write << std::endl;
+      std::cout << "Is ready to read?:" << dq->ready_to_read << " "
+                << "Is ready to write?:" << dq->ready_to_write << std::endl;
       std::cout << "Index: " << dq_index << " Size:" << dq->size << std::endl;
+
       for (int i = 0; i < 101; i++) {
         std::cout << dq->data[dq_index - 100 + i] << " ";
       }
       exit(-1);
     }
 
-    if (counter % 100000000 == 0) {
-      std::cout << "Processed " << counter / 1000000 << "M events" << std::endl;
+    if (counter % 100'000'000 == 0) {
+      std::cout << "Processed " << counter / 1'000'000 << "M events" << std::endl;
     }
   }
+
+}
+
+int main(int argc, char** argv) {
+  segment = new bip::fixed_managed_shared_memory(bip::open_or_create, "MySharedMemory", sizeof(uint64_t) *QSIZE *4, (void*)(1UL << 32));
+
+  // double buffering
+  dqA = segment->construct<double_queue_t>("DQ_A")(double_queue_t());
+  dqB = segment->construct<double_queue_t>("DQ_B")(double_queue_t());
+  auto dataA = segment->construct<uint64_t>("DQ_Data_A")[QSIZE](uint64_t());
+  auto dataB = segment->construct<uint64_t>("DQ_Data_B")[QSIZE](uint64_t());
+  dqA->init(dataA);
+  dqB->init(dataB);
+  dq = dqB;
+  dq_other = dqA;
+  dq_data = dq->data;
+
+  consume_loop();
 }
